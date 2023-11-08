63487d61bf4ea65ab86cf794c23442b0a26b24bf
livtito <livtito@gmail.com>
2023-11-07 18:01:35 -0800

    HW-19

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
+
+
 function fade() {
 $('.preloader').fadeOut("slow");
+
 }
 setTimeout(fade, 3000);
+
+
+$(Document).ready(function(){
+    $(window).scroll(function(){
+
+        $(".slideup").each(function(){
+            var x = $(this).offset().top;
+
+            var y = $(window).scrollTop();
+                if (x < y + 800){
+                    $(this).addClass("slide");
+                }
+
+        });
+    });
+});
